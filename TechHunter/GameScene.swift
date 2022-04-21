a9ff7f5ccc220f391a252e0201344c70643867f8
Kenneth Johnson <kd123johnson@gmail.com>
2022-04-21 11:26:08 -0600

    Tech Hunter Need help with SkPhysics code as I can't figure it out

--- TechHunter/GameScene.swift
+++ TechHunter/GameScene.swift
@@ -23,6 +23,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     var background = SKSpriteNode(imageNamed: "Alien Desert")
     
     
+    enum CollisionType: UInt32
+    {
+        case player = 1
+        case playerWeapon = 2
+        case enemy = 4
+    }
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
@@ -38,7 +44,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate
             self.background.position = CGPoint(x:self.size.width / 2, y: self.size.height / 2)
             self.background.size = CGSize(width: self.size.width, height: self.size.height)
             self.addChild(self.background)
-            
         //Set sprites Zpotition
             self.gun.zPosition = 5
             self.addChild(self.gun)
@@ -50,25 +55,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate
             self.addChild(self.wormMonster)
             self.blackShot.zPosition = 5
             self.addChild(self.blackShot)
-            
-            
         }
-        //Link to the phyiscal nodes in Gamescene and at the same time a physics body and a border.
         
+        
+        //Link to the phyiscal nodes in Gamescene and at the same time a physics body and a border.
         do
         {
-            
-//            gun = self.childNode(withName: "sprite_gun00") as! SKSpriteNode
-//            print(self.childNode(withName: "sprite_gun00"))
-//            blackShot = self.childNode(withName: "Black_Shot00") as! SKSpriteNode
-//            print(self.childNode(withName: "Black_Shot00"))
-//            mechaLizard = self.childNode(withName: "Mecha_Lizzard0100") as! SKSpriteNode
-//            print(self.childNode(withName: "Mecha_Lizzard"))
-//            birdMonster = self.childNode(withName: "Bird_Monster00") as! SKSpriteNode
-//            print(self.childNode(withName: "Bird_Monster00"))
-//            wormMonster = self.childNode(withName: "Worm_Monster00") as! SKSpriteNode
-//            print(self.childNode(withName: "Worm_Monster00"))
-            //Add Body to the vector?
             gun.physicsBody?.categoryBitMask = 1
             blackShot.physicsBody?.categoryBitMask = 2
             mechaLizard.physicsBody?.categoryBitMask = 3
@@ -79,42 +71,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate
             
             self.physicsBody = border
             
-//            func mySpritetie1()
-//            {
-//                gun = SKSpriteNode(imageNamed: "gun")
-//                gun.name = kAnimalNodeName
-//                gun.position = CGPoint(x: 170, y: 35)
-//                gun.size = CGSize(width: 36, height: 100)
-//                gun.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 22, height: 100))
-//                gun.physicsBody?.affectedByGravity = false
-//                gun.physicsBody?.categoryBitMask = physicsCategory.Item
-//                gun.physicsBody?.collisionBitMask = physicsCategory.Boarder
-//                gun.physicsBody?.contactTestBitMask = physicsCategory.Monster
-//                self.addChild(gun)
-//            }
-//
-//            func mySpritetie2()
-//            {
-//                blackShot = SKSpriteNode(imageNamed: "blackShot")
-//                blackShot.name = kAnimalNodeName
-//                blackShot.position  = CGPoint(x: 250, y: 60)
-//                blackShot.size = CGSize(width: 90, height: 79)
-//                blackShot.physicsBody = SKPhysicsBody(circleOfRadius: 25)
-//                blackShot.physicsBody?.affectedByGravity = false
-//                blackShot.physicsBody?.categoryBitMask = physicsCategory.Item
-//                blackShot.physicsBody?.collisionBitMask = physicsCategory.Boarder
-//                blackShot.physicsBody?.contactTestBitMask = physicsCategory.Monster
-//                self.addChild(blackShot)
-//            }
-//            let joint  = SKPhysicsJointPin.joint(withBodyA: gun.physicsBody!, bodyB: blackShot.physicsBody!,
-//                                                 anchor: CGPoint(x: gun.frame.midX, y: blackShot.frame.minY))
-//            self.physicsWorld.add(joint)
+            func mySpritetie1()
+            {
+                gun = SKSpriteNode(imageNamed: "gun")
+                gun.position = CGPoint(x: 210.56, y: -139.947)
+                gun.size = CGSize(width: 36, height: 100)
+                gun.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 22, height: 100))
+                gun.physicsBody?.affectedByGravity = false
+
+                self.addChild(gun)
+            }
+
+            func mySpritetie2()
+            {
+                blackShot = SKSpriteNode(imageNamed: "blackShot")
+                blackShot.position  = CGPoint(x: 45.773, y: 94.676)
+                blackShot.size = CGSize(width: 90, height: 79)
+                blackShot.physicsBody = SKPhysicsBody(circleOfRadius: 25)
+                blackShot.physicsBody?.affectedByGravity = false
+
+                self.addChild(blackShot)
+            }
+ //           let joint = SKPhysicsJointPin.joint(withBodyA: blackShot, bodyB: gun, anchor: position)
+ //           scene!.physicsWorld.add(SKPhysicsJoint)
+ //           self.physicsWorld.add(joint)
             
         }
         
         
         
-        //Collin line 30
+        //override function
     }
     
     
@@ -140,7 +126,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     
     
     
-    
+//End of Game Scene
 }
     
     
